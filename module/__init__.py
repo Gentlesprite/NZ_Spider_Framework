efdcd86c34cabd444c19ffa3ce9af1ff7cf83b49
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2025-08-12 15:02:47 +0800

    v0.8 尝试修复actions错误。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- module/__init__.py
+++ module/__init__.py
@@ -11,4 +11,3 @@ __version__: str = '0.8'
 __copyright__: str = f'Copyright (C) 2024-2025 {AUTHOR}.'
 logger.add('history.log', rotation='5 MB', encoding='UTF-8', enqueue=True, retention='10 days')
 logger.info(f'{__copyright__} Version:{__version__}')
-print('{:-^100}'.format('本软件为B站UP主"我不是盘神"免费分享,免费使用,如果你付费获取那就是被骗了!'))
